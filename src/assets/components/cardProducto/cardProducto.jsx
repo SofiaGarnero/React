2406e4a7a71f93e2e3ad110931a553756ecfe8db
Julieta Sofia Garnero <jgarnero@accionpoint.com>
2024-07-06 23:14:29 -0300

    entrega final

--- src/assets/components/cardProducto/cardProducto.jsx
+++ src/assets/components/cardProducto/cardProducto.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
 
 const CardProducto = ({prInfo}) => {
     const{nombre, precio} = prInfo
+    const[contador, setContador] = useState(0)
+
+    const suma = () => {
+        setContador(contador+1)
+    }
+    const resta =() => {
+        if (contador > 1) setContador(contador-1)
+    }
+
 return(
     <div className='cardsProduct'>
          <img src={prInfo.imagen} alt={prInfo.nombre} />
         <h4>{nombre}</h4>
         <p>${precio}</p>
-        <button onClick={() => console.log(`Producto ${nombre} agregado `)}>Agregar</button>
+        <p>{contador}</p>
+        <button onClick={resta}>-</button>
+        <button onClick={suma}>-</button>
+
+        <button onClick={() =>console.log(`Producto ${nombre} agregado `)}>Agregar al carrito</button>
     </div>
 )
 }
